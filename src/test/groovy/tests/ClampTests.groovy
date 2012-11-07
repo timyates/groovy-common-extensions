051adb54618b09abd08e6418fb953e3d01b2bc6a
Tim Yates <tyates@picr.man.ac.uk>
2012-11-07 13:03:57 +0000

    Neatened up ClampTests

--- src/test/groovy/tests/ClampTests.groovy
+++ src/test/groovy/tests/ClampTests.groovy
@@ -9,18 +9,18 @@ public class ClampTests extends Specification {
     expect:
       expected == input.clamp( lower, upper )
     where:
-      input | lower | upper | expected
-       1   | 3   | 5   | 3
-       2   | 3   | 5   | 3
-       3   | 3   | 5   | 3
-       4   | 3   | 5   | 4
-       5   | 3   | 5   | 5
-       6   | 3   | 5   | 5
-       2.0 | 2.1 | 2.4 | 2.1
-       2.3 | 2.1 | 2.4 | 2.3
-       2.5 | 2.1 | 2.5 | 2.5
-       'a' | 'c' | 'e' | 'c'
-       'd' | 'c' | 'e' | 'd'
-       'f' | 'c' | 'e' | 'e'
+      input | lower | upper || expected
+        1   |   3   |   5   ||     3
+        2   |   3   |   5   ||     3
+        3   |   3   |   5   ||     3
+        4   |   3   |   5   ||     4
+        5   |   3   |   5   ||     5
+        6   |   3   |   5   ||     5
+        2.0 |   2.1 |   2.4 ||     2.1
+        2.3 |   2.1 |   2.4 ||     2.3
+        2.5 |   2.1 |   2.5 ||     2.5
+        'a' |   'c' |   'e' ||     'c'
+        'd' |   'c' |   'e' ||     'd'
+        'f' |   'c' |   'e' ||     'e'
   }
 }
